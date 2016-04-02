e56a45aa290125065c324ef3e2ecaa930dbbc303
Alho Poró <Alho Por>
2016-04-01 22:02:01 -0300

    Solução de protótipo

---
+++ Prototype Map And Char/Assets/Bla.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Bla : MonoBehaviour
+{
+    public GameObject Colisor;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Input.GetAxis("Horizontal") * Time.deltaTime, 0f, 0f);
+        transform.Translate(0f, Input.GetAxis("Vertical") * Time.deltaTime, 0f);
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name == "01_0")
+        {
+            Console.WriteLine("Demonio");
+        }
+    }
+}
---
+++ Prototype Map And Char/Assets/Replicate.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Replicate : MonoBehaviour
+{
+    public GameObject texture;
+    public float x;
+    public float y;
+    public int xRep;
+    public int yRep;
+    public float xDistance;
+    public float yDistance;
+    // Use this for initialization
+    void Start()
+    {
+        for (int x = 0; x < xRep; x++)
+        {
+            for (int y = 0; y < yRep; y++)
+            {
+                Instantiate(texture, new Vector3(x * xDistance, y * yDistance, 10), Quaternion.identity);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
